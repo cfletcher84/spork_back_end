cbe510f14cc8fee6057e3e3784a8a6d688ad21fe
cfletcher84 <chris@soldbyfletch.com>
2024-07-02 20:46:35 -0500

    Added POST route for tasks, Added GET for getting all tasks

--- routes/taskBP.py
+++ routes/taskBP.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
-from controllers.taskController import find_all
+from controllers.taskController import find_all, save
 
 task_blueprint = Blueprint('task_bp', __name__)
 
 task_blueprint.route('/', methods=['GET'])(find_all)
+task_blueprint.route('/', methods=['POST'])(save)
