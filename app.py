d7fe5fca990f28f4f1fa02c548877aad405f80a9
cfletcher84 <chris@soldbyfletch.com>
2024-07-10 09:45:38 -0500

    Added Date fuction

--- app.py
+++ app.py
@@ -30,11 +30,6 @@ if __name__ == '__main__':
     app = create_app('DevelopmentConfig')
 
     blueprint_config(app)
-
-    # with app.app_context():
-    #     db.drop_all()
-        # db.create_all()
-
     
     app.run(debug=True)
 
