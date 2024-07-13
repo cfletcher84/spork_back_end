ba3d532e0ecebe1fc94b8899f0569343c7c8b8d7
cfletcher84 <chris@soldbyfletch.com>
2024-07-13 11:33:20 -0500

    Added ProductionConfig

--- config.py
+++ config.py
@@ -3,3 +3,7 @@ import os
 class DevelopmentConfig:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or ''
     DEBUG = True
+
+class ProductionConfig:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or ''
+    DEBUG = True
