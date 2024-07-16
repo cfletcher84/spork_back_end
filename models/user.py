5c57875122d1e7f5ca92529877722ea45cb24f57
cfletcher84 <chris@soldbyfletch.com>
2024-07-16 14:43:39 -0500

    Added get task by user_id

--- models/user.py
+++ models/user.py
@@ -1,5 +1,19 @@
+# from database import db, Base
+# from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+# class User(Base):
+#     __tablename__ = "users"
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     first_name: Mapped[str] = mapped_column(db.String(255), nullable=False)
+#     last_name: Mapped[str] = mapped_column(db.String(255), nullable=False)
+#     email: Mapped[str] = mapped_column(db.String(300), unique=True)
+#     password: Mapped[str] = mapped_column(db.String(500), nullable=False)
+#     spoons: Mapped[int] = mapped_column(nullable=False)  # ASk about constant value of
+#     # profile_pic: Mapped[str] = mapped_column(db.String(100), nullable=True)
+#     task: Mapped['Tasks'] = relationship(back_populates='user')
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from database import db, Base
-from sqlalchemy.orm import Mapped, mapped_column
 
 class User(Base):
     __tablename__ = "users"
@@ -8,6 +22,8 @@ class User(Base):
     last_name: Mapped[str] = mapped_column(db.String(255), nullable=False)
     email: Mapped[str] = mapped_column(db.String(300), unique=True)
     password: Mapped[str] = mapped_column(db.String(500), nullable=False)
-    spoons: Mapped[int] = mapped_column(nullable=False)  # ASk about constant value of 12
+    spoons: Mapped[int] = mapped_column(nullable=False)  # ASk about constant value of
     # profile_pic: Mapped[str] = mapped_column(db.String(100), nullable=True)
-    task: Mapped['Task'] = db.relationship(back_populates='user')
+    tasks: Mapped[list['Task']] = relationship('Task', back_populates='user') 
+    
+from models.task import Task
